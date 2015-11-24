f78c84a9a904ee1a24bf208a9dda096f17fcbff7
hamogu <hgunther@mit.edu>
2015-11-24 15:59:40 -0500

    MIT setup, move sorce specifics earlier to allow interfere with filtering in GTI construction

--- master.mk
+++ master.mk
@@ -1,12 +1,10 @@
-SUBDIRS = 0119700201  0120900201  0121920101  0134720801  0134721501  0134722001  0510780201 0119700301  0121500301  0134720101  0134720901  0134721601  0134722101  0510780401 0119700401  0121500401  0134720401  0134721001  0134721701  0510780101  0510780501
-
-#SUBDIRS = 0119700201  0121920101  0134720801  0134721501  0134722001  0510780201 0119700301  0121500301  0134720101  0134720901  0134721601  0134722101  0510780401 0119700401  0121500401  0134720401  0134721001  0134721701  0510780101  0510780501
+SUBDIRS = 0741680101  0741680201  0741680301
 
 
 
-ORIGIN = /data/hguenther/obs/XMM/scripts
+ORIGIN = /nfs/melkor/d1/guenther/soft/XMM-data-reduction-scripts
 SRCFILES = /data/hguenther/John/my_processing/makefiles
-FLARE = 0134721501
+FLARE = 
 
 .PHONY: subdirs $(SUBDIRS) noflare
 
@@ -22,10 +20,10 @@ $(SUBDIRS):
 	# Auto update makefile in subdir from master make
 	$(MAKE) -C $@ makefile -f $(ORIGIN)/master.mk
 	$(MAKE) -C $@ defaults.mk -f $(ORIGIN)/master.mk
-	#$(MAKE) -C $@ EPIC_prepare
-	#$(MAKE) -C $@ clean_EPIC
 	cp $(SRCFILES)/$@.src $@/
-	$(MAKE) -C $@ SRCFILE=$@.src EPIC
+	$(MAKE) -C $@ SRCFILE=$@.src EPIC_images
+	#$(MAKE) -C $@ clean_EPIC
+	#$(MAKE) -C $@ SRCFILE=$@.src EPIC
 
 # deal with those ObsIDs which need anoflare filtering and have special noflare src files
 # in this setup it only works if that is only one dir!
@@ -34,7 +32,6 @@ noflare : $(FLARE)
 	cp $(SRCFILES)/$<_noflare.src $</
 	$(MAKE) -C $< SRCFILE=$<_noflare.src EPIC
 
-#SRC=$@.src
 # in /media/MAX/moritz/obs/XMM/
 # make subdirs -f /data/hguenther/obs/XMM/scripts/master.mk -k -j 3
 # -j 3: Do parallel, but max 3 processes at a time. I have dual core, if I use much more, than it's swapping all the time.
